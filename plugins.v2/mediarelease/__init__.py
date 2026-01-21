c0ef8916e8d091af34746ffa313569329d784334
Sebastian Luo <65555676+lwore@users.noreply.github.com>
2026-01-21 15:54:02 +1000

    Update __init__.py

--- plugins.v2/mediarelease/__init__.py
+++ plugins.v2/mediarelease/__init__.py
@@ -29,7 +29,7 @@ class MediaRelease(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/thsrite/MoviePilot-Plugins/main/icons/mediarelease.png"
     # 插件版本
-    plugin_version = "1.3"
+    plugin_version = "1.4"
     # 插件作者
     plugin_author = "thsrite"
     # 作者主页
